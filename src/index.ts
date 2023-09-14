494e32a7d6cdec235f0c27a4265d53df44b55d52
Nikolay Kulikov <nikolay.kulikov@nrg-soft.com>
2023-09-14 20:40:59 +0300

    Checkbox init

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { ThemeState } from "./theme";
 
 export * from "./components/ProgressCircle";
 export * from "./components/Typography";
+export * from "./components/Checkbox";
 
 declare module "styled-components" {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
